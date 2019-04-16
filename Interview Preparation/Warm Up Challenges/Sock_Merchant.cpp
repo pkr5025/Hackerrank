85865a736ff189b6b8280e2ddc641ea943c38df6
pkr5025 <44106322+pkr5025@users.noreply.github.com>
2019-04-15 20:03:58 -0400

    Update Sock_Merchant.cpp

--- Interview Preparation/Warm Up Challenges/Sock_Merchant.cpp
+++ Interview Preparation/Warm Up Challenges/Sock_Merchant.cpp
@@ -1,4 +1,4 @@
-//Hackerrank Url
+//Hackerrank Problem URL
 //https://www.hackerrank.com/challenges/sock-merchant/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
 
 #include <bits/stdc++.h>
